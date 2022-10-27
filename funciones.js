38e2cfbd0c5e61b49cfad2c2f468fcbd454240ae
yessipr <yt.peralta16@gmail.com>
2022-10-27 11:33:26 -0500

    seccion9

---
+++ funciones.js
@@ -0,0 +1,5 @@
+
+//funcion de inicializacion para popovers
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
